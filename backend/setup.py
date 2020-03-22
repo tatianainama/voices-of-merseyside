e97cdd0ea1890be614236bbcb6e9501959c85244
david <davidventura27@gmail.com>
2020-03-22 21:33:55 +0100

    initial backend

---
+++ backend/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='backend',
+        version='0.1',
+        description='',
+        url='',
+        author='',
+        author_email='',
+        license='',
+        packages=['backend'],
+        zip_safe=False,
+        entry_points={
+            'console_scripts': [
+                'webserver = backend.app:start'
+                ],
+            },
+        install_requires=[
+            'Flask==1.1.1',
+            'Flask-RESTful==0.3.8',
+            'Flask-SQLAlchemy==2.4.1',
+            'psycopg2-binary==2.8.4',
+            'SQLAlchemy==1.3.15',
+            'waitress==1.4.3',
+            ])
