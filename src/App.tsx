1d9f123a055d667383c57439ef739742e8d29254
Tatiana Inama <tatii.oxo@gmail.com>
2020-04-16 14:12:48 +0200

    Set first page to intro.

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ type AnswersData = {
 
 const PAGES = [0, 1, 2, 3, 4];
 const App = () => {
-  const [ currentPage, setCurrentPage ] = useState(2);
+  const [ currentPage, setCurrentPage ] = useState(0);
   const [ exit, setExit ] = useState(false);
   const [ answers, saveAnswers ] = useState<AnswersData>({
     personalInformation: {
